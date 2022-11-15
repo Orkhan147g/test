06e7c4905e95e982c5f0311375610c7b46472d2d
Orkhan147g <117181967+Orkhan147g@users.noreply.github.com>
2022-11-15 14:19:28 +0400

    test

---
+++ Homework.cs
@@ -0,0 +1,96 @@
+﻿#region HW1
+
+//Console.WriteLine("Choose a number");
+//string numStr = Console.ReadLine();
+//int n = Convert.ToInt32(numStr);
+
+//while (n % 2 == 0)
+//{
+//    n /= 2;
+//}
+//if (n == 1)
+//{
+//    Console.WriteLine("Quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("Quvveti deyil");
+//}
+
+
+
+#endregion
+
+#region HW2
+
+//Console.WriteLine("Choose a number");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Choose a number");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//for (int i = num1+1; i < num2; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+#endregion
+
+#region HW3
+
+//Console.WriteLine("Choose a number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int i = 70;
+//if (num > i)
+//{
+//    num = num - i;
+//    num *= 3;
+//}
+//else
+//{
+//    num -= i;
+//}
+//Console.WriteLine(num);
+
+#endregion
+
+#region HW4
+
+//int x = 13;
+//int y = 11;
+//int z = 19;
+
+//if (x > y && x > z)
+//{
+//    Console.WriteLine(x);
+//}
+//if (y > x && y > z)
+//{
+//    Console.WriteLine(y);
+//}
+//if (z > y && z > x)
+//{
+//    Console.WriteLine(z);
+//}
+
+#endregion
+
+#region HW5
+
+//Console.WriteLine("Choose a number");
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Choose a number");
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//int count = 0;
+//if (n % 2 == 0)
+//{
+//    n++;
+//}
+//for (int i = n; i <= m; i += 2)
+//{
+//    count++;
+//}
+
+//Console.WriteLine(count);
+
+#endregion
